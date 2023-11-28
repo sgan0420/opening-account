22102a04bee4a94de2197643eb253c9542f2bdf9
cjGanShijie <sgan0047@student.monash.edu>
2023-11-28 09:42:09 +0800

    session storage done

--- src/app/account-opening/account-opening.component.ts
+++ src/app/account-opening/account-opening.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -11,25 +11,25 @@ export class AccountOpeningComponent {
 
   parentForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.parentForm = this.fb.group({});
+    this.cdr.detectChanges();
+    console.log(this.parentForm);
+  }
 
   onSubmit(f: NgForm) {
-    console.log(f);
     if (this.parentForm.valid) {
-      // Submit the form
       console.log('Form submitted:', this.parentForm.value);
     } else {
       console.log('Form is invalid. Please fill out the required fields.');
     }
-  }
-
-  ngOnInit(): void {
-    this.parentForm = this.fb.group({});
+    sessionStorage.setItem('fatcaStatusData', JSON.stringify(this.parentForm.get('fatcaStatusForm').value));
   }
 
   isStepCompleted(form) {
     return form ? form.valid : false;
   }
-  
 
 }
--- src/app/fatca-status/fatca-status.component.ts
+++ src/app/fatca-status/fatca-status.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -13,9 +12,12 @@ export class FatcaStatusComponent implements OnInit {
   @Input() parentForm: FormGroup;
   fatcaStatusForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    
+    console.log(sessionStorage.getItem('fatcaStatusData'));
+    
     this.fatcaStatusForm = this.fb.group({
       citizenship: ['', Validators.required],
       birth: ['', Validators.required],
@@ -24,7 +26,12 @@ export class FatcaStatusComponent implements OnInit {
       tax: ['', Validators.required],
       taxResidencies: this.fb.array([]),
     });
+    
     this.parentForm.addControl('fatcaStatusForm', this.fatcaStatusForm);
+
+    const storedData = JSON.parse(sessionStorage.getItem('fatcaStatusData'));
+    this.fatcaStatusForm.patchValue(storedData);
+    
   }
 
   get taxResidencies() {
@@ -37,6 +44,27 @@ export class FatcaStatusComponent implements OnInit {
       tin: ['', Validators.required]
     });
     this.taxResidencies.push(taxResidency);
+  }
+
+  func() {
+    this.fatcaStatusForm.controls['citizenship'].valueChanges.subscribe((value) => {
+      console.log(value);
+    })
+    this.fatcaStatusForm.controls['birth'].valueChanges.subscribe((value) => {
+      console.log(value);
+    })
+    this.fatcaStatusForm.controls['address'].valueChanges.subscribe((value) => {
+      console.log(value);
+    })
+    this.fatcaStatusForm.controls['telephone'].valueChanges.subscribe((value) => {
+      console.log(value);
+    })
+    this.fatcaStatusForm.controls['tax'].valueChanges.subscribe((value) => {
+      console.log(value);
+    })
+    this.fatcaStatusForm.controls['taxResidencies'].valueChanges.subscribe((value) => {
+      console.log(value);
+    })
   }
 
   deleteTaxResidency(index: number) {
@@ -60,7 +88,6 @@ export class FatcaStatusComponent implements OnInit {
         text: 'Please note that due to restrictions and tax reporting requirements to U.S. persons, we are sorry to inform that we are unable to open accounts for an individual with U.S. citizenship/ U.S. Permanent Resident Status/ U.S. Taxpayer Identification Number (TIN) and/ or U.S. Residential/ Mailing Address. For further clarification, please contact us.',
       });
     }
-
   }
 
 }
